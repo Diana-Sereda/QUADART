2320867082bb59362b162fec055407dea642e805
Diana-Sereda <69309199+Diana-Sereda@users.noreply.github.com>
2020-09-14 16:50:17 +0530

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,21 @@
+//Cursor Effect
+let mouseCursor = document.querySelector(".cursor");
+let navLinks = document.querySelectorAll(".item");
+window.addEventListener('mousemove', cursor);
+
+function cursor(e) {
+    mouseCursor.style.top = e.pageY + 'px';
+    mouseCursor.style.left = e.pageX + 'px';
+}
+
+navLinks.forEach(link => {
+    link.addEventListener("mouseover", () => {
+        mouseCursor.classList.add("link-grow");
+        link.classList.add("hovered-link");
+    });
+    link.addEventListener("mouseleave", () => {
+        mouseCursor.classList.remove("link-grow");
+        link.classList.remove("hovered-link");
+    });
+});
+//------------------------
