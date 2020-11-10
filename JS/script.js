f38b232b01ff72958e0fe9361a1d78f019669fa8
Diana-Sereda <69309199+Diana-Sereda@users.noreply.github.com>
2020-11-10 23:02:39 +0530

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -34,6 +34,41 @@ $(".no-cursor").hover(function () {
 
 //Nav on scroll
 
+//var prevScrollpos = window.pageYOffset;
+//const header = document.querySelector("header");
+//const main = document.querySelector("main");
+//window.onscroll = function () {
+//    var currentScrollPos = window.pageYOffset;
+//    var $win = $(window);
+//    if (prevScrollpos > currentScrollPos) {
+//        document.getElementById("navbar").style.top = "0";
+//        header.classList.add("nav-scrolled");
+//        $("h4").removeClass("nav-text");
+//        $("path").addClass("fill");
+//        $("a").addClass("scrolled");
+//
+//    } else {
+//        document.getElementById("navbar").style.top = "-70px";
+//    }
+//    prevScrollpos = currentScrollPos;
+//};
+//
+//$(function () {
+//    var $win = $(window);
+//
+//    $win.scroll(function () {
+//        if ($win.scrollTop() === 0) {
+//            header.classList.remove("nav-scrolled");
+//            $("h4").addClass("nav-text");
+//            $("path").removeClass("fill");
+//            $("a").removeClass("scrolled");
+//        }
+//
+//    });
+//
+//});
+
+
 var prevScrollpos = window.pageYOffset;
 const header = document.querySelector("header");
 const main = document.querySelector("main");
@@ -68,107 +103,113 @@ $(function () {
     });
 
 });
-        //------------------------
+
+//------------------------
 
 
 
 
-        //Arrow gray-white
+//Arrow gray-white
 
-        $(".gray").hover(function () {
-                $(".wh-on-hover").css("display", "inline");
-                $(".dissapear").css("display", "none");
-            },
-            function () {
-                $(".wh-on-hover").css("display", "none");
-                $(".dissapear").css("display", "inline");
-            });
+$(".gray").hover(function () {
+        $(".wh-on-hover").css("display", "inline");
+        $(".dissapear").css("display", "none");
+    },
+    function () {
+        $(".wh-on-hover").css("display", "none");
+        $(".dissapear").css("display", "inline");
+    });
 
-        //------------------------
+//------------------------
 
-        // Arrow jump
+// Arrow jump
 
-        $(".j").hover(function () {
-            $(".j1").css("padding-bottom", "8px")
-                .css("transition", "0.2s");
-        }, function () {
-            $(".j1").css("padding-bottom", "0")
-                .css("transition", "0");
-        });
-        //--------
-        $(".j2").hover(function () {
-            $(".j3").css("padding-bottom", "8px")
-                .css("transition", "0.2s");
-        }, function () {
-            $(".j3").css("padding-bottom", "0")
-                .css("transition", "0");
-        });
-        //----------
-        $(".j4").hover(function () {
-            $(".j5").css("padding-bottom", "8px")
-                .css("transition", "0.2s");
-        }, function () {
-            $(".j5").css("padding-bottom", "0")
-                .css("transition", "0");
-        });
-        //-----------
-        $(".make-order-text h1").hover(function () {
-            $(".make-order-arrow").css("bottom", "0px")
-                .css("transition", "0.2s");
-        }, function () {
-            $(".make-order-arrow").css("bottom", "-10px")
-                .css("transition", "0");
-        });
+$(".j").hover(function () {
+    $(".j1").css("padding-bottom", "8px")
+        .css("transition", "0.2s");
+}, function () {
+    $(".j1").css("padding-bottom", "0")
+        .css("transition", "0");
+});
+//--------
+$(".j2").hover(function () {
+    $(".j3").css("padding-bottom", "8px")
+        .css("transition", "0.2s");
+}, function () {
+    $(".j3").css("padding-bottom", "0")
+        .css("transition", "0");
+});
+//----------
+$(".j4").hover(function () {
+    $(".j5").css("padding-bottom", "8px")
+        .css("transition", "0.2s");
+}, function () {
+    $(".j5").css("padding-bottom", "0")
+        .css("transition", "0");
+});
+//-----------
+$(".make-order-text h1").hover(function () {
+    $(".make-order-arrow").css("bottom", "0px")
+        .css("transition", "0.2s");
+}, function () {
+    $(".make-order-arrow").css("bottom", "-10px")
+        .css("transition", "0");
+});
 
-        //Make order window
-        $(".zakz").click(function () {
-            $("#make-order").css("display", "inherit");
-        }); $(".close").click(function () {
-            $("#make-order").css("display", "none");
-        });
+//Make order window
+$(".zakz").click(function () {
+    $("#make-order").css("display", "inherit");
+});
+$(".close").click(function () {
+    $("#make-order").css("display", "none");
+});
 
-        //Arrow move
-        $(".back").hover(function () {
-            $(".back-to").css("position", "relative");
-            $(".back-to").css("left", "-5px");
-        }, function () {
-            $(".back-to").css("position", "relative");
-            $(".back-to").css("left", "0");
-        });
+//Arrow move
+$(".back").hover(function () {
+    $(".back-to").css("position", "relative");
+    $(".back-to").css("left", "-5px");
+}, function () {
+    $(".back-to").css("position", "relative");
+    $(".back-to").css("left", "0");
+});
 
-        //Footer icons on hover
-
-
-        $(".media-icon").on('mouseover', function (e) {
-            var hoveredIcon = $(e.target).attr('alt');
-            if (hoveredIcon == "behance") {
-                $(this).attr("src", "icons/Be%20(Yellow).svg");
-            } else if (hoveredIcon == "facebook") {
-                $(this).attr("src", "icons/Fb%20(Yellow).svg");
-            } else if (hoveredIcon == "instagram") {
-                $(this).attr("src", "icons/In%20(Yellow).svg");
-            } else if (hoveredIcon == "pinterest") {
-                $(this).attr("src", "icons/P%20(Yellow).svg");
-            }
-        });
-
-        $(".media-icon").on('mouseout', function (e) {
-            var hoveredIcon = $(e.target).attr('alt');
-            if (hoveredIcon == "behance") {
-                $(this).attr("src", "icons/Be.svg");
-            } else if (hoveredIcon == "facebook") {
-                $(this).attr("src", "icons/Fb.svg");
-            } else if (hoveredIcon == "instagram") {
-                $(this).attr("src", "icons/In.svg");
-            } else if (hoveredIcon == "pinterest") {
-                $(this).attr("src", "icons/P.svg");
-            }
-        });
-
-        //Parallax Effect
-        var rellax = new Rellax('.part1');
-        var rellax = new Rellax('.part2');
-        var rellax = new Rellax('.p-effect');
+//Footer icons on hover
 
 
-        //Main page
+$(".media-icon").on('mouseover', function (e) {
+    var hoveredIcon = $(e.target).attr('alt');
+    if (hoveredIcon == "behance") {
+        $(this).attr("src", "icons/Be%20(Yellow).svg");
+    } else if (hoveredIcon == "facebook") {
+        $(this).attr("src", "icons/Fb%20(Yellow).svg");
+    } else if (hoveredIcon == "instagram") {
+        $(this).attr("src", "icons/In%20(Yellow).svg");
+    } else if (hoveredIcon == "pinterest") {
+        $(this).attr("src", "icons/P%20(Yellow).svg");
+    }else if (hoveredIcon == "telegram") {
+        $(this).attr("src", "icons/Tg%20(Yellow).svg");
+    }
+});
+
+$(".media-icon").on('mouseout', function (e) {
+    var hoveredIcon = $(e.target).attr('alt');
+    if (hoveredIcon == "behance") {
+        $(this).attr("src", "icons/Be.svg");
+    } else if (hoveredIcon == "facebook") {
+        $(this).attr("src", "icons/Fb.svg");
+    } else if (hoveredIcon == "instagram") {
+        $(this).attr("src", "icons/In.svg");
+    } else if (hoveredIcon == "pinterest") {
+        $(this).attr("src", "icons/P.svg");
+    }else if (hoveredIcon == "telegram") {
+        $(this).attr("src", "icons/Tg.svg");
+    }
+});
+
+//Parallax Effect
+var rellax = new Rellax('.part1');
+var rellax = new Rellax('.part2');
+var rellax = new Rellax('.p-effect');
+
+
+//Main page
